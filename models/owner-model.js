de2927e20c5e1ccb73621766f231dc51af2e7158
zain <zainrajpootfd@gmail.com>
2024-10-04 12:47:15 +0500

    add models

---
+++ models/owner-model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    minLength: 3,
+    trim: true,
+  },
+  email: String,
+  password: String,
+  product: {
+    type: Array,
+    default: [],
+  },
+  picture: String,
+  gstin: String,
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
